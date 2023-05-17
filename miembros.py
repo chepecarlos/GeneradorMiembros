0c2df64e297311f7f1a71f8d424be6cdbf56bb3e
chepecarlos <chepecarlos@alswblog.org>
2023-05-17 17:11:45 -0600

    mejorando animacion

--- miembros.py
+++ miembros.py
@@ -5,13 +5,11 @@ from manim import *
 
 
 def cargarData(archivo):
-    # archivoData = FuncionesArchivos.UnirPath(ruta, archivo)
     if not os.path.exists(archivo):
-        logger.warning(f"No se control {archivo}")
+        print(f"No se control {archivo}")
         return None
     data = pd.read_csv(archivo)
     return data
-
 
 class miembros(Scene):
     def construct(self):
@@ -22,15 +20,23 @@ class miembros(Scene):
         txGracias = Text("Gracias por su apoyo", slant=ITALIC).scale(1.5)
         txGracias.shift(2.5 * UP)
 
-        avengers_and_thanos = ImageMobject("fondo.jpg").scale(0.1)
-        self.add(avengers_and_thanos)
-        self.play(FadeIn(avengers_and_thanos))
-
         self.play(Write(txGracias))
 
         for nombres in miembros["Miembro"]:
-            patrocinador = Text(nombres).scale(1.2)
-            self.play(FadeIn(patrocinador, shift=DOWN, Scale=0.66))
-            self.play(ReplacementTransform(patrocinador, patrocinador))
-            self.play(FadeOut(patrocinador, shift=DOWN * 2, Scale=1.5))
-        self.wait(1)
+            imagen = ImageMobject(f"fotos_miembros/{nombres}.jpg").scale(0.2)
+            self.add(imagen)
+
+            nombreMiembro = Text(nombres).scale(1.4)
+            nombreMiembro.move_to([0,-1.5,0])
+
+            border = nombreMiembro.copy()
+            border.set_stroke(color=BLUE, width=8)
+            self.add(border, nombreMiembro)
+            self.play(
+                       FadeIn(nombreMiembro, shift=DOWN, Scale=0.66), 
+                       FadeIn(imagen, shift=DOWN, Scale=0.66),
+                       FadeIn(border, shift=DOWN, Scale=0.66),)
+            
+            self.play(FadeOut(nombreMiembro, shift=DOWN * 2, Scale=1.5), FadeOut(imagen, shift=DOWN * 2, Scale=1.5))
+            return
+        self.wait(2)
